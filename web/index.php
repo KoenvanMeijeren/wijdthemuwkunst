8a3bb415e6aa93f083f160f4553ee48e925bf8e9
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2020-01-22 08:55:23 +0100

    move dot env loader to index.php

--- web/index.php
+++ web/index.php
@@ -2,8 +2,12 @@
 declare(strict_types=1);
 
 use App\Src\Core\App;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotEnv = Dotenv::createImmutable(START_PATH);
+$dotEnv->load();
 
 $app = new App();
 $app->run();
