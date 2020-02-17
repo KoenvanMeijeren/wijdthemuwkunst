69d0740045dd3f65c1d1bfb448e18063388e2b2b
Koen van Meijeren <kmeijere0303@student.landstede.nl>
2020-02-17 21:10:00 +0100

    add the base layout of the website

--- resources/layout/layout.view.php
+++ resources/layout/layout.view.php
@@ -1,174 +1,98 @@
 <?php
 
+use Domain\Admin\Settings\Models\Setting;
 use Src\Translation\Translation;
 
+$setting = new Setting();
 ?>
 <!DOCTYPE html>
 <html lang="<?= Translation::DUTCH_LANGUAGE_CODE ?>">
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
     <!-- SEO -->
     <meta name="description" content="Christelijk koor Wijdt Hem Uw Kunst">
     <meta name="keywords" content="Christelijk koor, Harderwijk">
     <meta name="author" content="Christelijk koor Wijdt Hem Uw Kunst">
 
-    <!-- Font -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600"
-          rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap"
-          rel="stylesheet">
-
     <!-- Bootstrap -->
-    <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
 
     <!-- Font awesome -->
-    <script src="https://kit.fontawesome.com/ec953a682d.js"
-            crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ec953a682d.js" crossorigin="anonymous"></script>
+
+    <!-- Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Pacifico">
 
     <!-- Theme -->
-    <link rel="stylesheet" href="/css/theme.css">
+    <link rel="stylesheet" href="css/main.css" />
 
     <title><?= $data['title'] ?? 'Undefined' ?></title>
 
     <link rel="icon" href="/images/logo.png">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg m-0 p-0">
-    <div class="container">
-        <div class="d-flex flex-grow-1">
-            <a class="navbar-brand logo" href="/">
-<!--                <img src="/web/assets/images/logo.png"-->
-<!--                     alt="Wijdt Hem uw kunst logo" width="100%">-->
-                <h1 class="m-2">
-                    Christelijk koor <br>
-                    Wijdt Hem Uw Kunst
-                </h1>
-            </a>
 
-            <button class="navbar-toggler button-unstyled"
-                    type="button" id="nav-icon"
-                    data-toggle="collapse" data-target="#navbar"
-                    aria-controls="navbar" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-        </div>
+<!-- Header -->
+<header id="header">
+    <div class="inner">
+        <a href="/" class="logo">Wijdt Hem Uw Kunst</a>
+        <nav id="nav">
+            <a href="/">Home</a>
+            <a href="/over-ons">Over ons</a>
+            <a href="/koor">Het koor</a>
+            <a href="/#concerten">Concerten</a>
+        </nav>
+    </div>
+</header>
+<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
 
-        <div class="collapse navbar-collapse flex-grow-1" id="navbar">
-            <ul class="navbar-nav ml-3">
-                <li class="nav-item active">
-                    <a class="nav-link active" href="#home">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#over-ons">
-                        Over ons
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#koor">
-                        Het koor
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#concerten">
-                        Concerten
-                    </a>
-                </li>
+<div class="page">
+    <?= $content ?? '' ?>
+</div>
+
+<!-- Footer -->
+<section id="footer">
+    <div class="inner">
+        <header>
+            <h2>Neem contact op</h2>
+        </header>
+        <form method="post" action="#">
+            <div class="field half first">
+                <label for="name">Naam</label>
+                <input type="text" name="name" id="name" />
+            </div>
+            <div class="field half">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" />
+            </div>
+            <div class="field">
+                <label for="message">Bericht</label>
+                <textarea name="message" id="message" rows="6"></textarea>
+            </div>
+            <ul class="actions">
+                <li><input type="submit" value="Bericht verzenden" class="alt" /></li>
             </ul>
+        </form>
+        <div class="copyright">
+            &copy; <?= $setting->getByKey('copyright_tekst') ?>
         </div>
     </div>
-</nav>
+</section>
 
-<?= $content ?? '' ?>
-
-<footer class="page-footer font-small bg-light pt-4">
-    <div class="container page text-center text-md-left">
-        <div class="row">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-2 mb-3">
-                <h5 class="font-beyond">Sociale media</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <i class="fab fa-facebook"></i>
-                        <a href="#facebook">
-                            Facebook
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fab fa-instagram"></i>
-                        <a href="#instagram">
-                            Instagram
-                        </a>
-                    </li>
-                    <li>
-                        <i class="fab fa-youtube"></i>
-                        <a href="#youtube">
-                            Youtube
-                        </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-sm-3 mb-3">
-                <h5 class="font-beyond">Secretariaat</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <i class="far fa-envelope"></i>
-                        Postbus 3, 3456 AB Harderwijk
-                    </li>
-                    <li>
-                        <i class="fas fa-envelope"></i>
-                        info@wijdthemuwkunst.nl
-                    </li>
-                    <li>
-                        <i class="fas fa-phone"></i>
-                        (0341) 123456
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-sm-2 mb-3">
-                <h5 class="font-beyond">Informatie</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <i class="fas fa-euro-sign"></i>
-                        IBAN:
-                    </li>
-                    <li>
-                        <i class="fas fa-building"></i>
-                        Kvk:
-                    </li>
-                </ul>
-            </div>
-            <div class="col-sm-2"></div>
-        </div>
-    </div>
-
-    <div class="footer-copyright text-center py-3">
-        © 2019 Copyright: Wijdt Hem uw kunst
-    </div>
-
-    <!-- Jquery -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
 
-    <!-- Bootstrap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<!-- Bootstrap -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-    <!-- Site JS -->
-    <script src="/js/default.js"></script>
-</footer>
+<!-- Site JS -->
+<script src="js/skel.min.js"></script>
+<script src="js/util.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
