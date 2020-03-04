36570b96ead3526453ed9d29bbe0aa76bc1214d6
koenvanmeijeren <koenvanmeijeren@develto.nl>
2020-03-04 19:39:46 +0100

    fix error 500 view styling bugs

--- resources/partials/http/500-error.view.php
+++ resources/partials/http/500-error.view.php
@@ -1,1 +1,6 @@
-<p><?= $description ?? '' ?></p>
+<div class="container page">
+    <div class="mt-5 mb-5">
+        <h1><?= $title ?? '' ?></h1>
+        <p><?= $description ?? '' ?></p>
+    </div>
+</div>
