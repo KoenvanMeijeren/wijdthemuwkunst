e56001c4cc17fc86f573f0cff0763167152a8bad
koenvanmeijeren <koenvanmeijeren@develto.nl>
2020-04-05 13:41:53 +0200

    Optimize the user actions

--- App/Domain/Admin/Accounts/User/Actions/UpdateUserDataAction.php
+++ App/Domain/Admin/Accounts/User/Actions/UpdateUserDataAction.php
@@ -4,45 +4,19 @@ declare(strict_types=1);
 
 namespace Domain\Admin\Accounts\User\Actions;
 
-use Domain\Admin\Accounts\User\Models\User;
-use Src\Action\FormAction;
-use Src\Core\Request;
-use Src\Session\Session;
-use Src\State\State;
-use Src\Translation\Translation;
-use Src\Validate\form\FormValidator;
 
-final class UpdateUserDataAction extends FormAction
+use App\Domain\Admin\Accounts\User\Actions\BaseUserAction;
+
+final class UpdateUserDataAction extends BaseUserAction
 {
-    private Session $session;
-    private User $user;
-
-    private string $name;
-
-    public function __construct(User $user)
-    {
-        $request = new Request();
-        $this->session = new Session();
-        $this->user = $user;
-
-        $this->name = $request->post('name');
-    }
-
     /**
      * @inheritDoc
      */
-    protected function handle(): bool
+    protected function prepareAttributes(): void
     {
-        $this->user->update($this->user->getId(), [
-            'account_name' => $this->name
-        ]);
-
-        $this->session->flash(
-            State::SUCCESSFUL,
-            Translation::get('admin_edited_account_successful_message')
-        );
-
-        return true;
+        $this->attributes = [
+            'account_name' => $this->name,
+        ];
     }
 
     /**
@@ -50,11 +24,8 @@ final class UpdateUserDataAction extends FormAction
      */
     protected function validate(): bool
     {
-        $validator = new FormValidator();
+        $this->validator->input($this->name, 'Naam')->isRequired();
 
-        $validator->input($this->name, 'Naam')
-            ->isRequired();
-
-        return $validator->handleFormValidation();
+        return $this->validator->handleFormValidation();
     }
 }
--- App/Domain/Admin/Accounts/User/Actions/UpdateUserPasswordAction.php
+++ App/Domain/Admin/Accounts/User/Actions/UpdateUserPasswordAction.php
@@ -4,60 +4,22 @@ declare(strict_types=1);
 
 namespace Domain\Admin\Accounts\User\Actions;
 
+use App\Domain\Admin\Accounts\User\Actions\BaseUserAction;
 use Domain\Admin\Accounts\Account\Models\Account;
-use Domain\Admin\Accounts\Repositories\AccountRepository;
-use Domain\Admin\Accounts\User\Models\User;
-use Src\Action\FormAction;
-use Src\Core\Request;
-use Src\Session\Session;
-use Src\State\State;
-use Src\Translation\Translation;
-use Src\Validate\form\FormValidator;
 
-final class UpdateUserPasswordAction extends FormAction
+final class UpdateUserPasswordAction extends BaseUserAction
 {
-    private Session $session;
-    private User $user;
-
-    private string $currentPassword;
-    private string $newPassword;
-    private string $confirmationPassword;
-
-    private AccountRepository $account;
-
-    public function __construct(User $user)
-    {
-        $request = new Request();
-        $this->session = new Session();
-
-        $this->user = $user;
-        $this->account = new AccountRepository($user->getAccount());
-
-        $this->currentPassword = $request->post('currentPassword');
-        $this->newPassword = $request->post('newPassword');
-        $this->confirmationPassword = $request->post(
-            'confirmationPassword'
-        );
-    }
-
     /**
      * @inheritDoc
      */
-    protected function handle(): bool
+    protected function prepareAttributes(): void
     {
-        $this->user->update($this->account->getId(), [
+        $this->attributes = [
             'account_password' =>  (string) password_hash(
                 $this->newPassword,
                 Account::PASSWORD_HASH_METHOD
             )
-        ]);
-
-        $this->session->flash(
-            State::SUCCESSFUL,
-            Translation::get('admin_edited_account_successful_message')
-        );
-
-        return true;
+        ];
     }
 
     /**
@@ -65,16 +27,14 @@ final class UpdateUserPasswordAction extends FormAction
      */
     protected function validate(): bool
     {
-        $validator = new FormValidator();
-
-        $validator->input($this->currentPassword)
+        $this->validator->input($this->currentPassword)
             ->passwordIsVerified($this->account->getPassword());
 
-        $validator->input($this->newPassword)
+        $this->validator->input($this->newPassword)
             ->isRequired()
             ->passwordIsEqual($this->confirmationPassword)
             ->passwordIsNotCurrentPassword($this->currentPassword);
 
-        return $validator->handleFormValidation();
+        return $this->validator->handleFormValidation();
     }
 }
