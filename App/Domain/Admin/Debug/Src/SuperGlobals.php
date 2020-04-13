a1b18f0d8c84070d4fc67d05dbc33eb009bbd220
koenvanmeijeren <koenvanmeijeren@develto.nl>
2020-04-13 13:55:15 +0200

    Remove old DataTable.php

--- App/Domain/Admin/Debug/Src/SuperGlobals.php
+++ App/Domain/Admin/Debug/Src/SuperGlobals.php
@@ -4,12 +4,12 @@
 namespace App\Domain\Admin\Debug\Src;
 
 
+use App\Src\DataTable\DataTable;
 use Cake\Chronos\Chronos;
 use Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException;
 use Src\Core\Cookie;
 use Src\Session\Session;
 use Src\Translation\Translation;
-use Support\DataTable;
 use Support\DateTime;
 
 final class SuperGlobals
@@ -18,11 +18,13 @@ final class SuperGlobals
     {
         $table = new DataTable();
 
-        $table->addHead(
+        $table->addHead([
             Translation::get('table_row_header')
-        );
+        ]);
         foreach (headers_list() as $value) {
-            $table->addRow($value);
+            $table->addRow([
+                $value
+            ]);
         }
 
         return $table->get('undefined');
@@ -33,27 +35,30 @@ final class SuperGlobals
         $table = new DataTable();
         $session = new Session();
 
-        $table->addHead(
+        $table->addHead([
             Translation::get('table_row_key'),
             Translation::get('table_row_value')
-        );
+        ]);
         foreach (session_get_cookie_params() as $key => $data) {
             if ($key === 'lifetime' && $session->exists('createdAt')) {
                 $createdAt = new Chronos($session->get('createdAt'));
                 $expiredAt = $createdAt->addSeconds($data);
                 $lifetime = new Chronos();
 
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $lifetime->diffInMinutes($expiredAt) . ' minuten'
-                );
+                ]);
             } elseif (is_bool($data)) {
-                $table->addRow($key, $data ? 'true' : 'false');
+                $table->addRow([
+                    $key,
+                    $data ? 'true' : 'false'
+                ]);
             } else {
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $data
-                );
+                ]);
             }
         }
 
@@ -65,10 +70,10 @@ final class SuperGlobals
         $session = new Session();
         $table = new DataTable();
 
-        $table->addHead(
+        $table->addHead([
             Translation::get('table_row_key'),
             Translation::get('table_row_value')
-        );
+        ]);
         foreach (array_keys($_SESSION) as $key) {
             if ($key === 'CSRF') {
                 continue;
@@ -77,15 +82,15 @@ final class SuperGlobals
             if ($key === 'createdAt') {
                 $dateTime = new DateTime(new Chronos($session->get($key)));
 
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $dateTime->toDateTime()
-                );
+                ]);
             } else {
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $session->get($key)
-                );
+                ]);
             }
         }
 
@@ -97,30 +102,30 @@ final class SuperGlobals
         $cookie = new Cookie();
         $table = new DataTable();
 
-        $table->addHead(
+        $table->addHead([
             Translation::get('table_row_key'),
             Translation::get('table_row_value')
-        );
+        ]);
         foreach (array_keys($_COOKIE) as $key) {
             if ($key === $cookie->get('sessionName')
                 && $cookie->exists($cookie->get('sessionName'))
             ) {
-                $table->addRow(
+                $table->addRow([
                     'sessie cookie',
                     $_COOKIE[$cookie->get('sessionName')]
-                );
+                ]);
             }
 
             try {
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $cookie->get($key)
-                );
+                ]);
             } catch (WrongKeyOrModifiedCiphertextException $e) {
-                $table->addRow(
+                $table->addRow([
                     $key,
                     $_COOKIE[$key]
-                );
+                ]);
             }
         }
 
